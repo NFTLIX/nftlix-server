dbb50d87f40545b7b7e7cdad6fc37ae5081aff82
CHO Min Ho <chominho96@naver.com>
2024-08-27 16:07:38 +0900

    feat: add project

---
+++ src/main/java/skku/nftlix_server/NftlixServerApplication.java
@@ -0,0 +1,13 @@
+package skku.nftlix_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftlixServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftlixServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skku/nftlix_server/NftlixServerApplicationTests.java
@@ -0,0 +1,13 @@
+package skku.nftlix_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftlixServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
