4fa286fd320457a109c68000ae456f7378ce84f7
CHO Min Ho <chominho96@naver.com>
2024-09-11 19:23:57 +0900

    feat: add multiple nft response

--- src/main/java/skku/nftlix_server/nft/dto/response/NftResponse.java
+++ src/main/java/skku/nftlix_server/nft/dto/response/NftResponse.java
@@ -3,7 +3,6 @@ package skku.nftlix_server.nft.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
-import skku.nftlix_server.nft.Nft;
 
 public record NftResponse(
         @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
