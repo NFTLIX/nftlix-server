3f98d95d1b1d9c0be8866c19c0f9d85a3e05d0ec
CHO Min Ho <chominho96@naver.com>
2024-09-11 14:34:20 +0900

    feat: add order entity

---
+++ src/main/java/skku/nftlix_server/order/Order.java
@@ -0,0 +1,27 @@
+package skku.nftlix_server.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import skku.nftlix_server.core.base.BaseEntity;
+
+@Entity
+@Getter
+@Table(name = "nft_order")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+
+    private String nftId;
+    private String memberId;
+
+    private Order(String nftId, String memberId) {
+        this.nftId = nftId;
+        this.memberId = memberId;
+    }
+
+    public static Order createOrder(String nftId, String memberId) {
+        return new Order(nftId, memberId);
+    }
+}
