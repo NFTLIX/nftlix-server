266825fd5554906e237624481e1fba1bd0f4df35
CHO Min Ho <chominho96@naver.com>
2024-09-11 14:20:24 +0900

    feat: add base entity

---
+++ src/main/java/skku/nftlix_server/core/base/BaseEntity.java
@@ -0,0 +1,27 @@
+package skku.nftlix_server.core.base;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "last_modified_at")
+    @LastModifiedDate
+    private Instant lastModifiedAt;
+
+}
