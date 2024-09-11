ab6582066e12152e79033430f4820d620401bd01
CHO Min Ho <chominho96@naver.com>
2024-09-11 14:01:50 +0900

    feat: add member access methods

--- src/main/java/skku/nftlix_server/member/repository/MemberRepository.java
+++ src/main/java/skku/nftlix_server/member/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package skku.nftlix_server.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import skku.nftlix_server.member.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, String> {
+
+    Optional<Member> findByLoginId(String loginId);
+    boolean existsByLoginId(String loginId);
 }
