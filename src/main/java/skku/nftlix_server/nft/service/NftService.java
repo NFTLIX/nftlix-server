d5f85d56a76ea0825a58a519f4e1b767dc04c7f2
CHO Min Ho <chominho96@naver.com>
2024-09-11 18:41:43 +0900

    feat: add create nft service

--- src/main/java/skku/nftlix_server/nft/service/NftService.java
+++ src/main/java/skku/nftlix_server/nft/service/NftService.java
@@ -3,13 +3,25 @@ package skku.nftlix_server.nft.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import skku.nftlix_server.member.Member;
+import skku.nftlix_server.nft.Nft;
+import skku.nftlix_server.nft.dto.request.NftRequest;
+import skku.nftlix_server.nft.dto.response.ImageResponse;
+import skku.nftlix_server.nft.dto.response.NftResponse;
 import skku.nftlix_server.nft.exception.NftNotFoundException;
 import skku.nftlix_server.nft.repository.NftRepository;
 import skku.nftlix_server.util.BashService;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -19,10 +31,57 @@ public class NftService {
 
     private final NftRepository nftRepository;
     private final BashService bashService;
+    private final RestTemplate restTemplate = new RestTemplate();
 
     private final String NODE_COMMAND = "node";
     private final String NFT_MINT_DIRECTORY = "/home/ubuntu/nft-mint/";
     private final String NFT_MINT_FILE = "scripts/mint-nft.js";
+
+
+    public NftResponse createNft(NftRequest request, MultipartFile image, Member member) throws IOException {
+
+        String id = UUID.randomUUID().toString();
+        String apiUrl = "https://ai.nftlix.store/api/v1/" + request.filter() + '/';
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        // 이미지 파일을 MultipartBody에 추가
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("image", new org.springframework.core.io.ByteArrayResource(image.getBytes()) {
+            @Override
+            public String getFilename() {
+                return image.getOriginalFilename(); // 파일명 설정
+            }
+        });
+        body.add("description", request.description());
+        body.add("name", request.name());
+        body.add("token_id", id);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        // API로 POST 요청 전송
+        ResponseEntity<ImageResponse> response
+                = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, ImageResponse.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            ImageResponse responseBody = response.getBody();
+            return new NftResponse(Nft.createNft(
+                    id,
+                    member.getId(),
+                    request.name(),
+                    request.description(),
+                    request.price(),
+                    request.royalty(),
+                    request.privilege(),
+                    responseBody.convertedImageUrl(),
+                    responseBody.originalImageUrl(),
+                    responseBody.metadataUrl()
+            ).getId());
+        } else {
+            throw new IllegalArgumentException("exception occurred in nftlix ai api");
+        }
+    }
 
     public void mint(String id) {
         String metadataUrl =
